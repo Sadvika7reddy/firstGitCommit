68e211a609454586d344b9de0418ca661897a019
sadvika7reddy <sadvika7reddy>
2022-11-06 20:13:36 +0530

    day1

--- sample.js
+++ sample.js
@@ -1,3 +1,7 @@
-console.log('HI');
-Console.log('Second  CheckIn');
-console.log('third checkin');
+const header=document.getElementById('header-title');
+header.textContent='hello';
+header.style.borderBottom='solid 3px #000';
+const title=document.querySelector('.title');
+console.log(title);
+title.style.color='Red';
+title.style.fontWeight="bold";
