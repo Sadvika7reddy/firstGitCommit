743f1d0ae2ec8f06ae18af15bac9bb74261ba4a4
sadvika7reddy <sadvika7reddy>
2022-11-07 15:36:17 +0530

    day 2-1

--- sample.js
+++ sample.js
@@ -1,5 +1,11 @@
-const obj1=document.querySelector('.list-items');
-console.log(obj1);
-obj1.style.color='red';
-obj1.style.background='yellow';
-
+const items=document.querySelector('.list-group-item:nth-child(2)');
+items.style.background='green';
+const thirditem=document.querySelector('.list-group-item:nth-child(3)');
+thirditem.remove();
+const item=document.querySelectorAll('.list-group-item');
+item[1].style.color='red';
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].style.background='#f4f4f4';
+}
