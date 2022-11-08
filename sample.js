d62056c72ae9601903295afc2fbe97561c0b5454
sadvika7reddy <sadvika7reddy>
2022-11-08 17:46:51 +0530

    delete and edit

--- sample.js
+++ sample.js
@@ -1,34 +1,28 @@
- //const itemlist=document.querySelector('#items');
- //console.log(itemlist.parentNode);
- //itemlist.parentNode.style.background='#f4f4f4';
- //console.log(itemlist.parentElement);
- //console.log(itemlist.children);
- //itemlist.children[1].style.color='red';
- //console.log(itemlist.firstChild);
- //console.log(itemlist.firstElementChild);
- //console.log(itemlist.lastElementChild);
- //itemlist.lastElementChild.textContent='hello';
- //console.log(itemlist.nextSibling);
- //console.log(itemlist.previousElementSibling);
- var newDiv=document.createElement('ul');
- newDiv.className='hello';
- newDiv.id='hello1';
- newDiv.setAttribute('tittle','hello div');
-var newDivText=document.createTextNode('hello world');
-newDiv.appendChild(newDivText);
-var container=document.querySelector('header .container');
-var h1=document.querySelector('header h1');
-
- console.log(newDiv);
- container.insertBefore(newDiv,h1);
- parentNode=document.getElementById('items');
- const hello=<li>hello world</li>
- parentNode.appendFirstChild(hello);
- //parentNode.innerhtml='<li>item 1</li><li>item 2</li><li>item 3</li>';
- //parentNode.innerhtml='<li>hello world</li>'+parentNode.innerhtml;
-
-
-
-
-
-
+ var form=document.getElementById('addForm');
+ var itemList=document.getElementById('items');
+ form.addEventListener('submit', addItem);
+ itemList.addEventListener('click',removeItem);
+ function addItem(e)
+ {
+    e.preventDefault();
+    console.log(1);
+    var newItem=document.getElementById('item').value;
+    var li=document.createElement('li');
+    li.className='list-group-item';
+    li.appendChild(document.createTextNode(newItem));
+    var deletebtn=document.createElement('button');
+    deletebtn.className='btn btn-danger btn-sm float-right delete';
+    deletebtn.appendChild(document.createTextNode('X'));
+    li.appendChild(deletebtn);
+    itemList.appendChild(li);
+ }
+ function removeItem(e)
+ {
+    if(e.target.classList.contains('delete'));
+    {
+        if(confirm('are you sure'));{
+            var li=e.target.parentElement;
+    itemList.removeChild(li);
+ }
+}
+ }
