0acedc09ccd65e258963ad794613630f3d6df43e
sadvika7reddy <sadvika7reddy>
2022-11-10 07:04:46 +0530

    scale the apps

--- sad.js
+++ sad.js
@@ -3,6 +3,7 @@
 const myform=document.querySelector('#my-form');
 const nameinput=document.querySelector('#name');
 const emailinput=document.querySelector('#email');
+const userList=document.querySelector('#users');
 myform.addEventListener('submit',onsubmit);
 function onsubmit(e)
 {
@@ -12,7 +13,15 @@ function onsubmit(e)
         email:emailinput.value
     }
     let myObj=JSON.stringify(userDetails)
-    localStorage.setItem('call',myObj);
+    localStorage.setItem('username'+emailinput.value,myObj);
     let obj1=JSON.parse(localStorage.getItem("call"));
     console.log(obj1);
+    const li=document.createElement('li');
+    li.appendChild(document.createTextNode(`${nameinput.value} ${emailinput.value}`));
+    userList.appendChild(li);
+
+
 }
+
+
+
