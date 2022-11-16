7560ad65e930e378d773356303c4c283dc8fa01e
sadvika7reddy <sadvika7reddy>
2022-11-16 16:12:50 +0530

    event listener

---
+++ new.js
@@ -0,0 +1,8 @@
+document.getElementById('click me').addEventListener('click',function xyz()
+{
+    console.log('hi');
+})
+document.getElementById('click me').addEventListener('click',function xyz()
+{
+    console.log('DOM has loaded');
+})
