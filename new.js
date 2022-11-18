d1131893e4448b2c547b1e85883a1b509f74543e
sadvika7reddy <sadvika7reddy>
2022-11-18 18:34:50 +0530

    call back

--- new.js
+++ new.js
@@ -1,31 +1,31 @@
-studentobj = {
-
-	'yash': 26,
-
-	'vaibhav': 24,
-
-	'rajesh' : 25,
-
-	}
-    let count=0;
-
-    function findaverageage(studentobj){
-        let sum=0;
-
-        const keyarr=Object.keys(studentobj);
-        keyarr.forEach(key=>{
-             sum=sum+studentobj[key];
-            count++;
-
-        })
-        const averageAge=sum/count;
-        
-    
-        
-    
-        return averageAge;
-    
-    }
-    console.log(findaverageage(studentobj));
-    
-    
+posts=[
+    {title:'post one',body:'the body is one',create:new Date().getTime()},
+    {title:'post two',body:'the body is two',create:new Date().getTime()}
+];
+let interval;
+function getpost()
+{
+    clearInterval(interval);
+    interval=setInterval(()=>{
+    let output='';
+    posts.forEach((post)=> {
+        output+=`<li>${post.title} at ${(new Date().getTime()-post.create)/1000} by seconds</li>`;
+    });
+    document.body.innerHTML=output;
+},1000);
+}
+function createPost(post,callback){
+    setTimeout(()=>{
+        posts.push(post);
+        callback();
+    },2000)
+}
+function creat4ePost(post,callback){
+    setTimeout(()=>{
+        posts.push(post);
+        callback();
+    },3000)
+}
+createPost({title:'post three',body:'the body is three',create:new Date().getTime()},getpost);   
+createPost({title:'post four',body:'the body is four',create:new Date().getTime()},getpost);   
+       
